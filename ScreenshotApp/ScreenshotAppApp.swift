20659b97f528642ebc5110129eeb40638829dcac
yzbpxx <643840734@qq.com>
2024-11-20 14:21:52 +0800

    Initial Commit

---
+++ ScreenshotApp/ScreenshotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotAppApp.swift
+//  ScreenshotApp
+//
+//  Created by yzb on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenshotAppTests/ScreenshotAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScreenshotAppTests.swift
+//  ScreenshotAppTests
+//
+//  Created by yzb on 2024/11/20.
+//
+
+import Testing
+
+struct ScreenshotAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
